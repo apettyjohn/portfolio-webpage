93d271ffa08d45c68dd09f89b3af0ab578ee5331
Adam Pettyjohn <apettyjohn7@gmail.com>
2020-05-12 17:55:33 -0600

    Added Font Awesome Icons

--- js/app.js
+++ js/app.js
@@ -4,5 +4,13 @@ const width = document.width;
 
 document.addEventListener('DOMContentLoaded', () => {
     
-    
+    document.querySelector('#face').addEventListener('animationend', () => {
+        navbar = document.getElementsByClassName('navbar')
+        console.log(navbar)
+        for (var item = 0; item < navbar.length; item++) {
+            navbar[item].style.visibility = 'visible';
+        }
+        
+    });
+    document.querySelector('#face').style.animationPlayState = 'running';
 })
