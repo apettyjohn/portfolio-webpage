9e2df0aa0bf547e14f633553b4e787c0273b0550
Adam Pettyjohn <apettyjohn7@gmail.com>
2020-05-13 07:33:22 -0600

    Moved navbar to left side

--- js/app.js
+++ js/app.js
@@ -3,14 +3,18 @@ const width = document.width;
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    face = document.querySelector('#face');
+    face.style.animationName = 'shrink';
+    face.style.animationPlayState = 'running';
     document.querySelector('#face').addEventListener('animationend', () => {
-        navbar = document.getElementsByClassName('navbar')
-        console.log(navbar)
-        for (var item = 0; item < navbar.length; item++) {
-            navbar[item].style.visibility = 'visible';
-        }
+        document.querySelector('#container').style.width = '12%';
+
+        face.style.animationName = '';
+        face.style.width = '100%';
+        face.style.marginTop = '0px';
+
+        navbar = document.querySelector('.navbar')
+        navbar.style.visibility = 'visible';
         
-    });
-    document.querySelector('#face').style.animationPlayState = 'running';
+    });  
 })
