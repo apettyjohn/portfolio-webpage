86f5260a62e80db0efe66e94c9e04e4b1779e7a4
Adam Pettyjohn <apettyjohn7@gmail.com>
2020-05-15 09:23:48 -0600

    Added menu expand function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 const height = document.height;
 const width = document.width;
 
@@ -6,7 +7,9 @@ window.addEventListener('DOMContentLoaded', () => {
     assignGridAreas();
     stackPanels();
 
-    document.querySelector('#flag-text').addEventListener('click', hideShowFlag());
+    document.querySelector('#flag-text').addEventListener('click', () => {
+        hideShowFlag(event)
+    });
     document.querySelector('#flag-text').addEventListener('mouseenter', () => {
         arrowAnimation(0.5, 8)
     });
@@ -72,5 +75,50 @@ function arrowAnimation(duration, distance){
 }
 
 function hideShowFlag() {
-    
+    let leftDist = getComputedStyle(document.querySelector('#flag')).getPropertyValue('left');
+    let menuWidth = getComputedStyle(document.querySelector('#container')).getPropertyValue('width');
+    console.log(leftDist, menuWidth);
+    const time = 1;
+    switch (leftDist) {
+        case "0px":
+            expandMenu(time, 1);
+        case menuWidth:
+            collapseMenu(time);
+        default:
+            console.log("menu is in an inbetween state");
+    }
 }
+
+function expandMenu(time, count){
+
+    const menu = document.querySelector('#container')
+    const menuStyles = getComputedStyle(menu);
+    const flag = document.querySelector('#flag')
+    const flagStyles = getComputedStyle(flag);
+    const menuWidth = Number(menuStyles.getPropertyValue('width').slice(0,menuStyles.getPropertyValue('width').length - 2)) + Number((2 * menuStyles.getPropertyValue('padding').slice(0,menuStyles.getPropertyValue('padding').length - 2)));
+    let flagDist = Number(flagStyles.getPropertyValue('left').slice(0,flagStyles.getPropertyValue('left').length - 2));
+    let distance = Number(menuStyles.getPropertyValue('left').slice(0,menuStyles.getPropertyValue('left').length - 2));
+    const maxVelocity = 4;
+    const timeFrame = 5;
+    const iterations = (time * 1000) / timeFrame;
+    if (count <= menuWidth / maxVelocity) {
+        increment = count * (maxVelocity / (menuWidth / maxVelocity));
+    } else if (count > menuWidth / maxVelocity) {
+        increment = count * (-1 * maxVelocity / (menuWidth / maxVelocity)) + (2 * maxVelocity);
+    };
+    
+
+    console.log(distance, count, increment);
+
+    if (distance >= 0 || increment <= 0){
+        menu.style.setProperty('left', "0px");
+        flag.style.setProperty('left', (menuWidth.toString() + "px"));
+        return;
+    } else if (distance < 0){
+        menu.style.setProperty('left', ((distance + increment).toString() + "px"));
+        flag.style.setProperty('left', ((flagDist + increment).toString() + "px"));
+    };
+
+    setTimeout(function () {expandMenu(time, count + 1)}, timeFrame);
+}
+
